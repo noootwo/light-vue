160ff05d862b94e96b26cd089b5d915c66c7ce02
NoTwoBoy <1244476905@qq.com>
2024-07-03 02:02:05 +0800

    feat(compiler-core): support parse full element with text and expression

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,14 +1,22 @@
 import { NodeTypes } from "./ast";
 
-const Delimiter = {
-  interpolationOpen: "{{",
-  interpolationClose: "}}",
-};
+enum Delimiter {
+  interpolationOpen = "{{",
+  interpolationClose = "}}",
+  elementStartOpen = "<",
+  elementEndOpen = "</",
+  elementClose = ">",
+}
+
+enum TagTypes {
+  Start,
+  End,
+}
 
 export function baseParse(content: string) {
   const context = createParseContext(content);
 
-  return createRoot(parseChildren(context));
+  return createRoot(parseChildren(context, []));
 }
 
 function createRoot(children) {
@@ -17,16 +25,56 @@ function createRoot(children) {
   };
 }
 
-function parseChildren(context) {
+let i = 0;
+function parseChildren(context, ancestors) {
   const nodes: any = [];
 
-  if (context.source.startsWith(Delimiter.interpolationOpen)) {
-    const node = parseInterpolation(context);
+  while (!isEnd(context, ancestors) && i++ < 10) {
+    let node;
+    if (context.source.startsWith(Delimiter.interpolationOpen)) {
+      node = parseInterpolation(context);
+    } else if (/^<\/?[a-z]/i.test(context.source)) {
+      node = parseElement(context, ancestors);
+    }
+
+    if (!node) {
+      node = parseText(context);
+    }
 
     nodes.push(node);
   }
 
   return nodes;
+}
+
+function isEnd(context, ancestors) {
+  const s = context.source;
+  if (!s) return true;
+
+  if (s.startsWith(Delimiter.elementEndOpen)) {
+    for (let i = ancestors.length - 1; i >= 0; i--) {
+      if (isContextStartsWithEndTag(context, ancestors[i])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function isContextStartsWithEndTag(context, tag) {
+  return (
+    context.source
+      .slice(0, Delimiter.elementEndOpen.length + tag.length + 1)
+      .toLowerCase() ===
+    (Delimiter.elementEndOpen + tag + Delimiter.elementClose).toLowerCase()
+  );
+}
+
+function parseTextData(context, length) {
+  const data = context.source.slice(0, length);
+  advanceBy(context, length);
+  return data;
 }
 
 function parseInterpolation(context) {
@@ -36,9 +84,11 @@ function parseInterpolation(context) {
     closeDelimiter,
     openDelimiter.length
   );
-  const content = context.source.slice(openDelimiter.length, closeIndex);
 
-  advanceBy(context, closeIndex + closeDelimiter.length);
+  advanceBy(context, openDelimiter.length);
+  const rawContent = parseTextData(context, closeIndex - 2);
+  const content = rawContent.trim();
+  advanceBy(context, closeDelimiter.length);
 
   return {
     type: NodeTypes.INTERPOLATION,
@@ -46,6 +96,55 @@ function parseInterpolation(context) {
       type: NodeTypes.SIMPLE_EXPRESSION,
       content,
     },
+  };
+}
+
+function parseTag(context, type: TagTypes) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  if (type === TagTypes.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
+}
+
+function parseElement(context, ancestors) {
+  const element: any = parseTag(context, TagTypes.Start);
+  ancestors.push(element.tag);
+  element.children = parseChildren(context, ancestors);
+  const endTag = ancestors.pop();
+
+  if (isContextStartsWithEndTag(context, endTag)) {
+    parseTag(context, TagTypes.End);
+  } else {
+    throw new Error(`Unclosed tag: ${endTag}`);
+  }
+
+  return element;
+}
+
+function parseText(context) {
+  const end = [Delimiter.interpolationOpen, Delimiter.elementStartOpen];
+  let endIndex = context.source.length;
+
+  for (let i = 0; i < end.length; i++) {
+    const index = context.source.indexOf(end[i]);
+    if (index !== -1 && endIndex > index) {
+      endIndex = index;
+    }
+  }
+
+  const content = parseTextData(context, endIndex);
+
+  return {
+    type: NodeTypes.TEXT,
+    content,
   };
 }
 
--- src/compiler-core/tests/index.spec.ts
+++ src/compiler-core/tests/index.spec.ts
@@ -4,7 +4,7 @@ import { baseParse } from "../src/parse";
 describe("parse", () => {
   describe("interpolation", () => {
     it("happy path", () => {
-      const ast = baseParse("{{value}}");
+      const ast = baseParse("{{ value }}");
 
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.INTERPOLATION,
@@ -14,5 +14,88 @@ describe("parse", () => {
         },
       });
     });
+  });
+
+  describe("element", () => {
+    it("happy path", () => {
+      const ast = baseParse("<div></div>");
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: "div",
+        children: [],
+      });
+    });
+  });
+
+  describe("text", () => {
+    it("happy path", () => {
+      const ast = baseParse("some text");
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.TEXT,
+        content: "some text",
+      });
+    });
+  });
+
+  describe("full parse", () => {
+    it("happy path", () => {
+      const ast = baseParse("<div>hi {{ name }}</div>");
+
+      expect(ast.children).toStrictEqual([
+        {
+          type: NodeTypes.ELEMENT,
+          tag: "div",
+          children: [
+            {
+              type: NodeTypes.TEXT,
+              content: "hi ",
+            },
+            {
+              type: NodeTypes.INTERPOLATION,
+              content: {
+                type: NodeTypes.SIMPLE_EXPRESSION,
+                content: "name",
+              },
+            },
+          ],
+        },
+      ]);
+    });
+
+    it("nested element", () => {
+      const ast = baseParse("<div><p>hi</p>{{ name }}</div>");
+
+      expect(ast.children).toStrictEqual([
+        {
+          type: NodeTypes.ELEMENT,
+          tag: "div",
+          children: [
+            {
+              type: NodeTypes.ELEMENT,
+              tag: "p",
+              children: [
+                {
+                  type: NodeTypes.TEXT,
+                  content: "hi",
+                },
+              ],
+            },
+            {
+              type: NodeTypes.INTERPOLATION,
+              content: {
+                type: NodeTypes.SIMPLE_EXPRESSION,
+                content: "name",
+              },
+            },
+          ],
+        },
+      ]);
+    });
+  });
+
+  test.only("want throw error", () => {
+    expect(() => baseParse("<div><p></div>")).toThrow("Unclosed tag: p");
   });
 });
